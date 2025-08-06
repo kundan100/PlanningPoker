734eecf87c0012055b864e3ebb52e323c776717c
kundan100 <kundan100@yahoo.com>
2025-08-07 04:25:27 +0530

    add a sample styled component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,21 @@
+'use client'; // The 'use client'; directive is required because styled-components is a client-side library, and this file uses React Client Components.
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import styled from 'styled-components';
+
+const Title = styled.h1`
+  font-size: 2rem;
+  color: #0070f3;
+  text-align: center;
+  margin-top: 2rem;
+`;
 
 export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
+        <Title>Hello from Styled Components 👋</Title>
         <Image
           className={styles.logo}
           src="/next.svg"
